b9e1cb578a110815d3a43909640c150443d0b18b
x1234s <1908131339@qq.com>
2018-08-29 00:19:36 +0800

    init commit

---
+++ helloworld/src/main/java/edu/demo/HelloWorldMainApplication.java
@@ -0,0 +1,13 @@
+package edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMainApplication.class, args);
+    }
+
+}
